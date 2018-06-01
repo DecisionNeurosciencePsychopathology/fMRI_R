d06e5cca47cbb4cb41cd773af2cfaa28411d53fa
Jiazhou Chen <rgagechen@gmail.com>
2018-06-01 10:37:10 -0400

    New dir.filter option

--- make_sybolink_fsl_highlevel.R
+++ make_sybolink_fsl_highlevel.R
@@ -4,13 +4,23 @@
 ####################################################
 
 #This function can't be run in R Studio anymore because of Mac's inheritence policy
-#Alternatively, use command line to open Rstuido, 'open -a Rstudio' so profile will be inherented
-#Function also use fsl's FLIRT to create first level flirt matrix if needed; 
-#if made somewhere else, soft link to each .feat folder as "masktostandtransforms.mat" matrix must be in FLIRT style
 
 ########Start Function:
-son.prepare4secondlvl<-function(ssana.path=NULL,preproc.path=NULL,standardbarin.path="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii",
-  proc.name=NULL,taskname=NULL,overwrite=TRUE) {
+son.prepare4secondlvl<-function(ssana.path=NULL,    
+                                #Single Sub Analysis Folder, should contain ID/*.feat
+                                preproc.path=NULL,
+                                #Preproc folder, should contain ID/${taskname}/${proc.name}[0-9]
+                                standardbarin.path="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii",
+                                #Path to standard template, take either .nii or .nii.gz
+                                proc.name=NULL,
+                                #See preproc path comment, this should always followed by a number 
+                                taskname=NULL,
+                                #See preproc path comment, this should be the dir that contains multiple runs
+                                dir.filter=NULL,
+                                #Filter out some of the unwanted folders in ssana.path, important especailly for those contain .feat structure
+                                overwrite=TRUE
+                                #Logical Statement, if to overwirte existing link
+                                ) {
   if (is.null(ssana.path) | is.null(preproc.path) | is.null(standardbarin.path) | is.null(proc.name) | is.null(taskname)){stop("not enough info to run")}
 #Step 1: Step up parameters, but it's a function so do it outside please
   #Step 2: Go to find all the feat. directory:
@@ -18,7 +28,7 @@ son.prepare4secondlvl<-function(ssana.path=NULL,preproc.path=NULL,standardbarin.
   #Break them down&take out all old_template_results
   strsplit(featlist,split = "/")->s.featlist
   s.featlist.p<-lapply(s.featlist,function(x) {
-    if (any(x %in% "old_template_results")) {
+    if (any(x %in% dir.filter)) {
       x<-NULL
     } else {x}
   })
@@ -46,9 +56,12 @@ son.prepare4secondlvl<-function(ssana.path=NULL,preproc.path=NULL,standardbarin.
       st2<-system(paste0("mkdir -m 775 ", linkmap$destination[i]),intern = T)
     if (!file.exists(linkmap$originplace[i]))  {
       system("source ~/.bash_profile")
-      system(paste0("/usr/local/ni_tools/fsl/bin/flirt -in ",file.path(ssana.path,linkmap$id[i],linkmap$runword[i],"mask.nii.gz")," -ref ",
+      system(paste0("${FSLDIR}/bin/flirt -in ",file.path(ssana.path,linkmap$id[i],linkmap$runword[i],"mask.nii.gz")," -ref ",
                     standardbarin.path," -out /Volumes/bek/neurofeedback/.temp.nii -omat ",linkmap$originplace[i],
                     " -bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear"))
+      print(paste0("${FSLDIR}/bin/flirt -in ",file.path(ssana.path,linkmap$id[i],linkmap$runword[i],"mask.nii.gz")," -ref ",
+                   standardbarin.path," -out /Volumes/bek/neurofeedback/.temp.nii -omat ",linkmap$originplace[i],
+                   " -bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear"))
     }
     if (length(st2)==0) {
       #Make actual links now....
@@ -74,11 +87,25 @@ son.prepare4secondlvl<-function(ssana.path=NULL,preproc.path=NULL,standardbarin.
 ########End Function
 
 #####Actually run it:
-test<-son.prepare4secondlvl(
-                      ssana.path="/Volumes/bek/neurofeedback/sonrisa1/nfb/ssanalysis/fsl",
-                      preproc.path="/Volumes/bek/neurofeedback/sonrisa1/proc",
-                      standardbarin.path="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii",
-                      proc.name="nfb_proc",
-                      taskname<-"nfb",
-                      overwrite<-TRUE
+#####You can get all the warnings by assign the result of the function to an object, and exam the object: test<-son.prepare4secondlvl(...)
+son.prepare4secondlvl(
+                      ssana.path="/Volumes/bek/neurofeedback/sonrisa2/con_framing/ssanalysis/fsl",            
+                      preproc.path="/Volumes/bek/neurofeedback/sonrisa2/proc",                                
+                      standardbarin.path="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii", 
+                      dir.filter="SC_FirstLevels_oldTemplate",                                                
+                      proc.name="cf",                                                                         
+                      taskname<-"cf_proc",                                                                   
+                      overwrite<-TRUE                                                                         
                       )
+
+
+
+# Below are arguments for nfb:
+
+# ssana.path="/Volumes/bek/neurofeedback/sonrisa1/nfb/ssanalysis/fsl",
+# preproc.path="/Volumes/bek/neurofeedback/sonrisa1/proc",
+# standardbarin.path="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii",
+# proc.name="nfb",
+# taskname<-"nfb_proc",
+# dir.filter=c("old_template_results","old"),
+# overwrite<-TRUE
