935342509b4faa28c09840618d7c283824248f70
Jiazhou Chen <35745779+Jiazhouchen@users.noreply.github.com>
2018-07-31 02:48:41 +0100

    Update dnpl_utility.R

--- dnpl_utility.R
+++ dnpl_utility.R
@@ -439,6 +439,63 @@ plot_image_all<-function(rootpath=NULL,
 
 
 
+#####Group Lvl Func
+glvl_all_cope<-function(rootdir="/Volumes/bek/neurofeedback/sonrisa1/nfb/ssanalysis/fsl",
+                       outputdir="/Volumes/bek/neurofeedback/sonrisa1/nfb/grpanal/fsl",
+                       modelname="PE_8C_old",
+                       copestorun=1:8,
+                       paralleln=NULL
+) {
+  if ( is.null(modelname) ) {stop("Must specify a model name other wise it will be hard to find all copes")}
+
+#Ensure fsl are in path:
+fsl_2_sys_env()
+
+raw<-system(paste0("find ",file.path(rootdir,modelname,"*/average.gfeat")," -iname '*.feat' -maxdepth 2 -mindepth 1 -type d"),intern = T)
+strsplit(raw,split = "/") ->raw.split
+df.ex<-data.frame(ID=unlist(lapply(raw.split,function(x) {
+  x[grep("average.gfeat",x)-1]
+})),
+COPENUM=unlist(lapply(raw.split,function(x) {
+  x[grep("average.gfeat",x)+1]
+})),
+PATH=file.path(raw,"stats","cope1.nii.gz")
+)
+df.ex$COPENUM<-substr(df.ex$COPENUM,start=regexpr("[0-9]",df.ex$COPENUM),stop = regexpr(".feat",df.ex$COPENUM)-1)
+if (max(aggregate(COPENUM~ID,data = df.ex,max)$COPENUM)<max(copestorun)) {stop("HEY! There's not that many copes to run! Change argument!")}
+noIDpos<-which(aggregate(COPENUM~ID,data = df.ex,max)$COPENUM!=max(aggregate(COPENUM~ID,data = df.ex,max)$COPENUM) & aggregate(COPENUM~ID,data = df.ex,max)$COPENUM<max(copestorun))
+if (length(noIDpos)>0){
+noID<-aggregate(COPENUM~ID,data = df.ex,max)$ID[noIDpos]
+print(paste("This ID:",noID,"does not have enough COPES, will be removed from running...."))
+df.ex[which(!df.ex$ID %in% noID),]->df.ex
+} else {print("All Good!")}
+print("Now will run fslmerge and randomise, function will fail if FSL ENVIR is not set up. (Should not happen since it's guarded by func)")
+
+cope.fslmerge<-lapply(copestorun,function(x) {
+  outputroot<-file.path(outputdir,modelname,paste0("cope",x,"randomize_onesample_ttest"))
+  dir.create(outputroot, showWarnings = FALSE)
+  copefileconcat<-paste(df.ex$PATH[which(df.ex$COPENUM==x)],collapse = " ")
+  paste0("fslmerge -t ",outputroot,"/OneSamp4D"," ",
+         copefileconcat
+         ," \n ",
+         "randomise -i ",outputroot,"/OneSamp4D -o ",outputroot,"/OneSampT -1 -T -x -c 3"
+         )
+  })
+sink(file="group_lvl_log.txt",split=TRUE)
+if (!is.null(paralleln)){
+  cj1<-makeCluster(paralleln,outfile="")
+  NU<-parSapply(cj1,cope.fslmerge,function(x) {
+    message(paste0("Now running ",cope.fslmerge))
+    system(command = x,intern = T,ignore.stdout = F,ignore.stderr = F)
+  })
+  stopCluster(cj1)
+} else {lapply(cope.fslmerge,function(x) {
+  message(paste0("Now running ",cope.fslmerge))
+  system(command = x,intern = T,ignore.stdout = F,ignore.stderr = F)
+})
+}
+print("DONE")
+}
 
 
 
