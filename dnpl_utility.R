e569e6c401ea4a16fd303553563ca074d18bcb95
Jiazhou Chen <rgagechen@gmail.com>
2018-09-01 14:52:46 -0400

    update prep for 2nd lvl

--- dnpl_utility.R
+++ dnpl_utility.R
@@ -327,25 +327,9 @@ findbox<-function() {
   return(boxdir)
 }
 
-prepare4secondlvl<-function(ssana.path=NULL,    
-                                #Single Sub Analysis Folder, should contain ID/*.feat
-                                preproc.path=NULL,
-                                #Preproc folder, should contain ID/${taskname}/${proc.name}[0-9]
-                                standardbarin.path="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii",
-                                #Path to standard template, take either .nii or .nii.gz
-                                proc.name=NULL,
-                                #See preproc path comment, this should always followed by a number 
-                                taskname=NULL,
-                                #See preproc path comment, this should be the dir that contains multiple runs
-                                dir.filter=NULL,
-                                #Filter out some of the unwanted folders in ssana.path, important especailly for those contain .feat structure
-                                overwrite=TRUE,
-                                #Logical Statement, if to overwirte existing link
-                                outputmap=FALSE,
-                                #Logical Statement, if true to return linkmap
-                                paralleln=NULL
-                                #Number of parallel jobs on going
-) {
+prepare4secondlvl<-function(ssana.path=NULL,preproc.path=NULL,
+                            standardbarin.path="/Volumes/bek/Newtemplate_may18/fsl_mni152/MNI152_T1_2mm_brain.nii",
+                            proc.name=NULL,taskname=NULL,dir.filter=NULL,overwrite=TRUE,outputmap=FALSE,paralleln=NULL) {
   if (is.null(ssana.path) | is.null(preproc.path) | is.null(standardbarin.path) | is.null(proc.name) | is.null(taskname)){stop("not enough info to run")}
   #Step 1: Step up parameters, but it's a function so do it outside please
   #Step 2: Go to find all the feat. directory:
@@ -372,16 +356,12 @@ prepare4secondlvl<-function(ssana.path=NULL,
   linkmap$destination_standard<-file.path(ssana.path,linkmap$id,linkmap$runword,"reg_standard")
   linkmap$originplace<-file.path(ssana.path,linkmap$id,linkmap$runword,"masktostandtransforms.mat")
   
-  
-  
-  
   if (is.null(paralleln)) {
   #Step 5, make symb.link for each subject and each directory pair:
   for (i in 1:length(linkmap$id)) {
     if (overwrite) {
-      st1<-suppressWarnings(system(paste0("rm -r ", linkmap$destination[i]),intern = T))
-      #st1_5<-suppressWarnings(system(paste0("rm -r ", linkmap$destination_standard[i]),intern = T))
-      st1_9<-suppressWarnings(system(paste0("rm -r ", linkmap$originplace[i]),intern = T))
+      if (file.exists(linkmap$destination[i])) {file.remove(linkmap$destination[i])}
+      if (file.exists(linkmap$originplace[i])) {file.remove(linkmap$originplace[i])}
     }
     st2<-dir.create(showWarnings = F,path = linkmap$destination[i])
     if (!file.exists(linkmap$originplace[i]))  {
@@ -390,24 +370,19 @@ prepare4secondlvl<-function(ssana.path=NULL,
                     standardbarin.path," -out /Volumes/bek/neurofeedback/.temp.nii -omat ",linkmap$originplace[i],
                     " -bins 256 -cost corratio -searchrx -90 90 -searchry -90 90 -searchrz -90 90 -dof 12  -interp trilinear"))
     }
-    
-    #Make actual links now....
-    #invisible(
     if (!file.exists(file.path(linkmap$destination,"example_func2standard.mat")[i])){
       file.symlink(from = file.path(linkmap$originplace)[i],to = file.path(linkmap$destination,"example_func2standard.mat")[i])
       file.symlink(from = file.path(linkmap$originplace)[i],to = file.path(linkmap$destination,"standard2example_func.mat")[i])
       file.symlink(from = standardbarin.path,to = file.path(linkmap$destination,"standard.nii.gz")[i])
     } else {message("meh,already there, if you want to overwirite, do overwrite...")}
-  
-    #)
+    
   }} else {
     cluster_prep2ndlvl<-makeCluster(paralleln,outfile=file.path(argu$ssub_outputroot,argu$model.name,"prep42ndlvl_log.txt"),type = "FORK")
     NU<-parSapply(cluster_prep2ndlvl,1:length(linkmap$id),function(i) {
       fsl_2_sys_env()
       if (overwrite) {
-        st1<-suppressWarnings(system(paste0("rm -r ", linkmap$destination[i]),intern = T))
-        #st1_5<-suppressWarnings(system(paste0("rm -r ", linkmap$destination_standard[i]),intern = T))
-        st1_9<-suppressWarnings(system(paste0("rm -r ", linkmap$originplace[i]),intern = T))
+        if (file.exists(linkmap$destination[i])) {file.remove(linkmap$destination[i])}
+        if (file.exists(linkmap$originplace[i])) {file.remove(linkmap$originplace[i])}
       }
       st2<-dir.create(showWarnings = F,path = linkmap$destination[i])
       if (!file.exists(linkmap$originplace[i]))  {
