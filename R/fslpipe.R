aa7f45a2b9e52d90fc431ad4f0128278548fb58a
Jiazhou Chen <rgagechen@gmail.com>
2019-02-15 05:24:31 -0500

    save g

--- R/fslpipe.R
+++ R/fslpipe.R
@@ -25,6 +25,7 @@ if (is.null(argu$nprocess)){
 ###Initializing argu;
 argu$cfg<-cfg_info(cfgpath = argu$cfgpath)
 argu$dsgrid<-read.table(argu$gridpath,header = T,sep = c(","),stringsAsFactors = F,strip.white = T,skipNul = T)
+if(is.null(argu$dsgrid$AddNeg)){argu$dsgrid$AddNeg<-FALSE}
 argu$dsgrid$AddNeg<-as.logical(argu$dsgrid$AddNeg)
 if(is.null(argu$model.varinames)) {argu$model.varinames<-argu$dsgrid$name}
 #Version upgrade safe keeping
