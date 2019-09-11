495e792719ec70ebb7246fb03988ee9f6dee60e7
Jiazhou Chen <rgagechen@gmail.com>
2019-09-11 19:18:22 -0400

    tx

--- R/fslpipe.R
+++ R/fslpipe.R
@@ -108,7 +108,7 @@ fsl_pipe<-function(argu=NULL, #This is the arguments environment, each model sho
       setwd(workingdir)
       save.image(file = "curwd.rdata")
       writeLines("library(fslpipe);load(\"curwd.rdata\");eval(step1_cmd)",con = "temp.r")
-      pbs_torun<-pbs_default;pbs_torun$cmd<-"Rscript temp.r";pbs_torun$ppn<-argu$nprocess
+      pbs_torun<-get_pbs_default();pbs_torun$cmd<-"Rscript temp.r";pbs_torun$ppn<-argu$nprocess
       writeLines(do.call(pbs_cmd,pbs_torun),"pbs_temp.sh")
       
     } else {
