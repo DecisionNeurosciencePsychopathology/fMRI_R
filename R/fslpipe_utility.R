3baf4e84adae7120d26bd77b428e1f43960874d1
Jiazhou Chen <35745779+Jiazhouchen@users.noreply.github.com>
2019-11-08 14:36:00 -0500

    Update fslpipe_utility.R

--- R/fslpipe_utility.R
+++ R/fslpipe_utility.R
@@ -966,7 +966,9 @@ roi_getvalue<-function(grproot=argu$glvl_output,modelname=NULL,glvl_method="rand
 
   if(glvl_method=="FLAME") {
     fsffiles<-list.files(file.path(grproot,"fsf_files"),pattern = "*.fsf",full.names = T)
-    if(length(fsffiles)<maxcore  && length(fsffiles)<2){coresx<-length(fsffiles)}else{coresx<-4}
+    nx <- length(fsffiles)
+    if ( nx <= maxcore || nx <= 2) {coresx <-nx} else {coresx <- 4}
+    
     sharedproc<-parallel::makeCluster(coresx,outfile="",type = "FORK")
     all_copes_ls<-parallel::parLapply(cl=sharedproc,fsffiles,function(fsfdir){
       fsfout<-readfsf(fsfdir)
