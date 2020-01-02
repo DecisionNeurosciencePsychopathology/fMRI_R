a056a306a6bb423e6ef93550125612defd52e8bd
Jiazhou Chen <rgagechen@gmail.com>
2020-01-02 15:04:37 -0500

    udpate

--- R/fslpipe.R
+++ R/fslpipe.R
@@ -508,6 +508,8 @@ fsl_pipe<-function(argu=NULL, #This is the arguments environment, each model sho
   stepnow<-6
   if (is.null(argu$run_steps) | stepnow %in% argu$run_steps) {
     fsl_2_sys_env()
+    ss_rootdir <- file.path(argu$subj_outputroot,argu$model_name)
+    
     if(argu$lvl1_afnify){
       if(!exists("lvl2_featlist")) {
         lvl2_featlist<-system(paste0("find ",file.path(argu$subj_outputroot,argu$model_name)," -iname ","*output.feat"," -maxdepth 2 -mindepth 1 -type d"),intern = T)
